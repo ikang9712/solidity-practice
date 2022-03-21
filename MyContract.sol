07a1d3ad2efbe0a36721844697b6abf394415e26
Inho Kang <inhokang@Inhos-MacBook-Pro.local>
2022-03-21 19:48:24 -0400

    mapping implementation

--- MyContract.sol
+++ MyContract.sol
@@ -1,15 +1,19 @@
 pragma solidity 0.5.1;
 
 contract MyContract {
-    Person[] public people;
-    uint256 public peopleCount;
+    mapping(uint => Person) public people;
+    uint256 public peopleCount = 0;
+
     struct Person {
+        uint _id;
         string _firstName;
         string _lastName;
     }
 
     function addPerson(string memory _firstName, string memory _lastName) public {
-        people.push(Person(_firstName, _lastName));
         peopleCount += 1;
+        people[peopleCount] = Person(peopleCount, _firstName, _lastName);
+        
+    
     }
 }
