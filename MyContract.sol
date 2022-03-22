bcc945b03fe091606d377d21a08377304340bae9
Inho Kang <inhokang@Inhos-MacBook-Pro.local>
2022-03-21 20:05:23 -0400

    Owner setting, authority setting

--- MyContract.sol
+++ MyContract.sol
@@ -4,16 +4,29 @@ contract MyContract {
     mapping(uint => Person) public people;
     uint256 public peopleCount = 0;
 
+    address owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
     struct Person {
         uint _id;
         string _firstName;
         string _lastName;
     }
 
-    function addPerson(string memory _firstName, string memory _lastName) public {
-        peopleCount += 1;
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function addPerson(string memory _firstName, string memory _lastName) public onlyOwner {
         people[peopleCount] = Person(peopleCount, _firstName, _lastName);
-        
-    
+        incrementCount();
+    }
+
+    function incrementCount() internal {
+        peopleCount += 1;
     }
 }
